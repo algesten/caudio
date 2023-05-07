955bc4febf8f0954e3b45551af786ca5b47a0374
Martin Algesten <martin@algesten.se>
2023-05-07 17:20:32 +0200

    keep types in types

--- src/unit/mod.rs
+++ src/unit/mod.rs
@@ -1,7 +1,5 @@
 mod desc;
 pub use desc::{Description, Version};
 
-mod types;
-pub use types::{
-    EffectType, FormatConverterType, GeneratorType, IOType, MixerType, MusicDeviceType, Type,
-};
+pub mod types;
+pub use types::Type;
